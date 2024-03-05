6aeee9fdf0c58ae74ecc72f175682fdf46c7954c
Yuzhe_CN <zhilinlicn@gmail.com>
2024-03-05 13:30:14 +0100

    Empezando a definir las clases que van a intervenir. Primer dockerfile para el entorno de compilación.

---
+++ src/drone-manager/include/dron.hpp
@@ -0,0 +1,11 @@
+#ifndef __DRON_HPP__
+#define __DRON_HPP__
+
+class Dron {
+public:
+
+private:
+
+};
+
+#endif  
---
+++ src/drone-manager/include/route.hpp
@@ -0,0 +1,12 @@
+#ifndef __ROUTE_HPP__
+#define __ROUTE_HPP__
+
+class Route {
+
+public:
+
+private:
+
+};
+
+#endif
